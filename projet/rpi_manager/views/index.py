24d5d3b591aeae0211572ebcd2aca359cf04fa8d
pilt <pilt@ns3373005.ip-37-187-101.eu>
2020-10-28 17:34:05 +0100

    point 0 fonctionne

--- projet/rpi_manager/views/index.py
+++ projet/rpi_manager/views/index.py
@@ -12,43 +12,54 @@ def index(request):
     schedule = list(WaterSchedule.objects.filter(
         rpi_id = 1
     ))
-    temp = []
-    for i in range(len(schedule)):
-        temp = [schedule[i].begin, schedule[i].end]
-        schedule[i] = temp
-        temp = []
+#    temp = []
+#    for i in range(len(schedule)):
+#        temp = [schedule[i].begin, schedule[i].end]
+#        schedule[i] = temp
+#        temp = []
 
-    last_ph = int(Ph.objects.latest('date').value)
+
+    last_ph = Ph.objects.latest('date')
+
 
     context_dict = {
         "schedule_list" : schedule,
         "last_ph": last_ph,
     }
-    #reglages_json = json.dumps(context_dict, indent = 4)
-    reglages_json = json.dumps(context_dict, indent=4, sort_keys=True, default=str)
-    #good_ph = list(Ph.objects.filter(id=1))
-    #temp += "ph : " + str(good_ph[0].value)
-    print(reglages_json)
+
+    ###### prepare to send with websocket
+    temp = ""
+    for elm in context_dict["schedule_list"]:
+        temp += elm.begin.strftime("%H:%M") + " , " + elm.end.strftime("%H:%M") + " | "
+    good_ph = list(Ph.objects.filter(id=1))
+    temp += "ph : " + str(good_ph[0].value)
+    print(temp)
 
 
+    ####### One formulary have been submit #######
 
     if request.method == "POST":
         motor_form = ManualMode(request.POST)
         if motor_form.is_valid():
             channel_layer = get_channel_layer()
             
+            #reglages_json = json.dumps(context_dict, indent=4, sort_keys=True, default=str)
+        
             ####### This part is sending the message to the websocket in group call "group0"
             async_to_sync(channel_layer.group_send)(
                 "group0",
                 {
                     "type": "send_message",
-                    "message": reglages_json
+                    "message": temp
                 }
             )
-            #######
+
+        ####### formulary problem create a page here ######
         else:
             print("manual form aint valid")
 
+
+    ####### initiate the main screen with clean formularies ######
     else:
         motor_form = ManualMode()
 
