e165dd4395843c83f54294230c1f0e6ad5bb2961
ometeore <pilt64@hotmail.fr>
2021-07-04 18:12:11 +0200

    separate travis settings test 0

---
+++ projet/hydropo/travis_settings.py
@@ -0,0 +1,12 @@
+from . import settings
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
